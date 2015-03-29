f0d8def99f2a925eccd8712ca9b7608cb4234318
Takayuki MATSUOKA <takayuki.matsuoka@gmail.com>
2015-03-29 18:07:38 +0900

    Fix ${GH_PAGES_BRANCH} variable name

--- build.sh
+++ build.sh
@@ -15,17 +15,17 @@ jekyll build
 # cleanup
 rm -rf ../${GH_REPO}.${GH_PAGES_BRANCH}
 
-#clone `${GH_BRANCH}' branch of the repository using encrypted GH_TOKEN for authentification
-git clone -b ${GH_BRANCH} https://${GH_TOKEN}@github.com/${GH_USER}/${GH_REPO}.git ../${GH_REPO}.${GH_BRANCH}
+#clone `${GH_PAGES_BRANCH}' branch of the repository using encrypted GH_TOKEN for authentification
+git clone -b ${GH_PAGES_BRANCH} https://${GH_TOKEN}@github.com/${GH_USER}/${GH_REPO}.git ../${GH_REPO}.${GH_PAGES_BRANCH}
 
-# copy generated HTML site to `${GH_BRANCH}' branch
-cp -R _site/* ../${GH_REPO}.${GH_BRANCH}
+# copy generated HTML site to `${GH_PAGES_BRANCH}' branch
+cp -R _site/* ../${GH_REPO}.${GH_PAGES_BRANCH}
 
-# commit and push generated content to `${GH_BRANCH}' branch
+# commit and push generated content to `${GH_PAGES_BRANCH}' branch
 # since repository was cloned in write mode with token auth - we can push there
-cd ../${GH_REPO}.${GH_BRANCH}
+cd ../${GH_REPO}.${GH_PAGES_BRANCH}
 git config user.email "${GH_GIT_EMAIL}"
 git config user.name "${GH_GIT_NAME}"
 git add -A .
 git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
-git push --quiet origin ${GH_BRANCH} > /dev/null 2>&1 
+git push --quiet origin ${GH_PAGES_BRANCH} > /dev/null 2>&1 
